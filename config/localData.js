2484992729cb7da37e935ef57a0b33f550560992
Aaron Smith <aaron@web-presence.biz>
2025-05-07 09:53:39 -0700

    building data

---
+++ config/localData.js
@@ -0,0 +1,10 @@
+let config = [{
+    apiAddress: "restaurantData.json",
+    navLinks: ["home", "about", "menu", "contact"],
+    banners: ["banner.jpg", "banner2.jpg", "banner3.jpg"],
+    description: "We make the best food. Here is our story of three generations.",
+    address: "1060 W. Addison Ave. Chicago Il",
+    phone: "555-222-3434",
+    email: "info@eat.com"
+
+}]
