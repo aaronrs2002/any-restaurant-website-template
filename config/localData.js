66121f88800391c287887b49af962c2eb3f9f2b0
Aaron Smith <aaron@web-presence.biz>
2025-05-07 10:18:24 -0700

    building out data ideas

--- config/localData.js
+++ config/localData.js
@@ -1,8 +1,10 @@
 let config = [{
     apiAddress: "restaurantData.json",
     navLinks: ["home", "about", "menu", "contact"],
-    banners: ["banner.jpg", "banner2.jpg", "banner3.jpg"],
+    banners: [{ img: "img/banner.jpg", bannerHTML: "<h3>Welcome, We have food!</h3>" }, { img: "img/banner2.jpg", bannerHTML: "<h3>We Eat food!</h3>" }, { img: "img/banner3.jpg", bannerHTML: "<h3>Clean Your Plate here!!</h3>" },],
     description: "We make the best food. Here is our story of three generations.",
+    media: [{ type: "img", address: "https://imageurl.com" }, { type: "ytVideo", address: "MrsOwvHgW48" }, { type: "html", address: "anyHTML here" }, { type: "map", address: "1060 W. Addison Chicago, IL" }],
+    events: [],
     address: "1060 W. Addison Ave. Chicago Il",
     phone: "555-222-3434",
     email: "info@eat.com"
