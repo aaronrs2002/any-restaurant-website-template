32d2a458bb0c354692f84d407a14ea4c26955c8f
Aaron Smith <aaron@web-presence.biz>
2025-05-16 15:01:49 -0700

    background image css work2

--- config/localData.js
+++ config/localData.js
@@ -2,16 +2,17 @@ let config = [{
     apiAddress: "https://aaronrs2002.github.io/any-restaurant-website-template/config/restaurantData.json",
     navLinks: ["home", "about", "menu", "contact"],
     banners: [{
-        img: "https://lh3.googleusercontent.com/pw/AP1GczOBwQdAdhwl9dVKp8sBQVRC4K9tt1s6eMUAHbCbhzE99-ssaHA6Fkiuh1aXZfmkrAfcPudBezXsUagy58h39w8_pCJ8gWpRH6mPQR3fBsC9DRoTdMxsKajRiiZmzZrkMIapFE1aOQ0w-A0QwcVGVYmgbA=w1920-h504-s-no-gm?authuser=0",
+        img: "https://lh3.googleusercontent.com/pw/AP1GczPe94dFwMQfmde5grW-Q4ococo5GGLOSTnw6dSZ-rkj6S3yniZZZLb-tXJO_-Mz2hPzatxbZuMAMZo8wSycW2fBXwiCG8GXca8l7jHz5Sj1ZwJACpVInobVPE1dw0Z7I4ExNXrmxS7yo9RcypFN_BA2jQ=w1289-h967-s-no-gm?authuser=0",
         bannerHTML: "<h3>Welcome, We have food!</h3>"
-    },
+    } /*,
 
-    {
-        img: "https://lh3.googleusercontent.com/pw/AP1GczN3wxjCuIlIctnYKFq_FrhjOh62XCz3vknKnqrTloupVWgQp2_jmYYkZ2vUIFsJS5lfVyzcJEd9YtNC24XJx7y57j5ltx91zttH2cyTYRKIq3aUei-Y_znIBqCh7OM-opi1ZhrEHfwqt24eau5fOej11w=w640-h480-s-no-gm?authuser=0",
-        bannerHTML: "<h3>We Eat food!</h3>"
-    },
+       {
+            img: "https://lh3.googleusercontent.com/pw/AP1GczP3wGagJW-TcrobEdZJIqd7_WNV-r7KUz7jaNt9UiTXX-s1W28VdpBBZRmJMA3IlcMjk01o4f3ONXgD-W6GHtU84Ahiru3bkJ4OBSQFfOIm5M7cIfWzc5NrR5BluiveJDYrjxKv0-EeGUtIUx83z48l-Q=w1920-h504-s-no-gm?authuser=0",
+            bannerHTML: "<h3>We Eat food!</h3>"
+        },
+    
+        { img: "https://lh3.googleusercontent.com/pw/AP1GczOSI7IERkOe-5nMoE3WVUS4aKJswF_YzN7YVedAQQgQxGM_VXE6V6YF1ewS0MjHlYbvaKLdM9gHfdjRrtolsqQL0SGEXNbaCE6kMaZE5An2DlGxi4Hbh-BvxYRU-eNRFsZYmPLeAV9M2_RxcjvbRfDYdA=w645-h967-s-no-gm?authuser=0", bannerHTML: "<h3>Clean Your Plate here!!</h3>" }, */],
 
-    { img: "https://lh3.googleusercontent.com/pw/AP1GczOqw9RkmM5z1B7iDuvJOsuVQK1kugsxB_ufDFGXbt5amPucSke4hEtV5Dnv7jfgsqNulQ3ZgDY_mILwMFIRjDQyhdK6HqfAGrrq2AWxrN2wlfujtnIGQIF0Q23oK796DeeU6uMpZZHl_WyvgqCxY42c7w=w1289-h967-s-no-gm?authuser=0", bannerHTML: "<h3>Clean Your Plate here!!</h3>" },],
     about: "We make the best food. Here is our story of three generations.",
     media: [{ type: "img", address: "https://imageurl.com" }, { type: "ytVideo", address: "MrsOwvHgW48" }, { type: "html", address: "anyHTML here" }, { type: "map", address: "1060 W. Addison Chicago, IL" }],
     events: [],
