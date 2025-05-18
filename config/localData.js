4fceeeb6e55510ca57b1c619fdcf021ef6081bb6
Aaron Smith <aaron@web-presence.biz>
2025-05-18 09:24:00 -0700

    new post on bg images3

--- config/localData.js
+++ config/localData.js
@@ -4,14 +4,14 @@ let config = [{
     banners: [{
         img: "https://lh3.googleusercontent.com/pw/AP1GczPe94dFwMQfmde5grW-Q4ococo5GGLOSTnw6dSZ-rkj6S3yniZZZLb-tXJO_-Mz2hPzatxbZuMAMZo8wSycW2fBXwiCG8GXca8l7jHz5Sj1ZwJACpVInobVPE1dw0Z7I4ExNXrmxS7yo9RcypFN_BA2jQ=w1289-h967-s-no-gm?authuser=0",
         bannerHTML: "<h3>Welcome, We have food!</h3>"
-    } /*,
+    },
 
-       {
-            img: "https://lh3.googleusercontent.com/pw/AP1GczP3wGagJW-TcrobEdZJIqd7_WNV-r7KUz7jaNt9UiTXX-s1W28VdpBBZRmJMA3IlcMjk01o4f3ONXgD-W6GHtU84Ahiru3bkJ4OBSQFfOIm5M7cIfWzc5NrR5BluiveJDYrjxKv0-EeGUtIUx83z48l-Q=w1920-h504-s-no-gm?authuser=0",
-            bannerHTML: "<h3>We Eat food!</h3>"
-        },
-    
-        { img: "https://lh3.googleusercontent.com/pw/AP1GczOSI7IERkOe-5nMoE3WVUS4aKJswF_YzN7YVedAQQgQxGM_VXE6V6YF1ewS0MjHlYbvaKLdM9gHfdjRrtolsqQL0SGEXNbaCE6kMaZE5An2DlGxi4Hbh-BvxYRU-eNRFsZYmPLeAV9M2_RxcjvbRfDYdA=w645-h967-s-no-gm?authuser=0", bannerHTML: "<h3>Clean Your Plate here!!</h3>" }, */],
+    {
+        img: "https://lh3.googleusercontent.com/pw/AP1GczNF_20NPayR8Ge4mD6GrHgnQqSQ9ecfOlo07wrEmxWPvRO_-GM9x-1qWWxpz9HhoAR4xskHJzSp9usquCCz8bKlL7otCU0Bia4MXuwPJSpY5uf8K4Q_cbwdq-2oD8ZGEDwkJUjGlYT9fVuQYHgb3QNI7A=w1703-h967-s-no-gm?authuser=0",
+        bannerHTML: "<h3>We Eat food!</h3>"
+    },
+
+    { img: "https://lh3.googleusercontent.com/pw/AP1GczMb0049t_cbpMtkVENokZYwW48GYULguC1WkT0l9InMhqZtMjyIHShNpNaRiR0cHw2ZkIxZD9vgdeuQ1Cn3NrOtLy9SRvgeiXlF0ZpAGr7BuEv7zXmxve7KLkPNlcRfBOadEQt8y8O4DM3SatF297FCfQ=w1724-h967-s-no-gm?authuser=0", bannerHTML: "<h3>Clean Your Plate here!!</h3>" },],
 
     about: "We make the best food. Here is our story of three generations.",
     media: [{ type: "img", address: "https://imageurl.com" }, { type: "ytVideo", address: "MrsOwvHgW48" }, { type: "html", address: "anyHTML here" }, { type: "map", address: "1060 W. Addison Chicago, IL" }],
